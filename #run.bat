d8007e07e2797bded9a5b3b2e6d88b6b1031f59c
Aaron <amserrano1337@gmail.com>
2022-02-13 15:14:16 +0800

    Server file update

--- #run.bat
+++ #run.bat
@@ -1,3 +1,7 @@
 @echo off
-set path="C:\Program Files\Eclipse Adoptium\jdk-8.0.312.7-hotspot\bin"
+git pull
+REM set path="C:\Program Files\Eclipse Adoptium\jdk-8.0.312.7-hotspot\bin"
 java -Xmx6G -Xmn512M -jar forge-1.12.2-14.23.5.2860.jar nogui
+git add .
+git commit -m "Server file update"
+git push origin master
