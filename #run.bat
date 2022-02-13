754005ad765bdacd4c735f7991fdd6c49c34d1ce
Aaron <amserrano1337@gmail.com>
2022-02-13 15:23:32 +0800

    Server file update

--- #run.bat
+++ #run.bat
@@ -1,7 +1,13 @@
 @echo off
+echo Checking for changes in Github...
 git pull
-REM set path="C:\Program Files\Eclipse Adoptium\jdk-8.0.312.7-hotspot\bin"
+timeout /t 5
+cls
+echo Running server...
 java -Xmx6G -Xmn512M -jar forge-1.12.2-14.23.5.2860.jar nogui
+cls
+echo Pushing new changes to Github...
 git add .
 git commit -m "Server file update"
 git push origin master
+timeout /t 5
