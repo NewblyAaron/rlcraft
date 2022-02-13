f934abce6de2c3f565998997c59a537725b3d848
Aaron <amserrano1337@gmail.com>
2022-02-13 15:31:43 +0800

    Server file update

--- #run.bat
+++ #run.bat
@@ -4,10 +4,13 @@ git pull
 timeout /t 5
 cls
 echo Running server...
+echo To stop the server, please use the 'stop' command.
+timeout /t 2 /NOBREAK
 java -Xmx6G -Xmn512M -jar forge-1.12.2-14.23.5.2860.jar nogui
 cls
-echo Pushing new changes to Github...
+echo Pushing new changes to Github.
+pause
 git add .
 git commit -m "Server file update"
-git push origin main
+git push
 timeout /t 5
